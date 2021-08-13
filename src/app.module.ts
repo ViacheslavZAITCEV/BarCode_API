e1cca0af4275420e14a64e66aaa8d17a38e891f2
Viacheslav ZAITCEV <zaitcev@mail.fr>
2021-08-13 11:30:43 +0200

    BD connection with typegoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose'
+import { TypegooseModule } from 'nestjs-typegoose'
 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRootAsync({
+    TypegooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get('CONNECTION_BD')
+        uri: configService.get('CONNECTION_BD'),
+        ...{ useNewUrlParser: true, useUnifiedTopology: true }
       })
     })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule { }
